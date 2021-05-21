5e2db0d1d2d4b0b4666709637b15dcbc5a040bd6
2ndwave-Kobayashi <kobayashi@2ndwave.jp>
2021-05-21 11:14:30 +0900

    単位マスタ：一覧から詳細への遷移処理作成

--- app/src/main/java/com/example/fixturesmanage/ui/unit/UnitFragment.kt
+++ app/src/main/java/com/example/fixturesmanage/ui/unit/UnitFragment.kt
@@ -4,25 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.fixturesmanage.R
+import com.example.fixturesmanage.adapter.UnitAdapter
+import com.example.fixturesmanage.dao.UnitDao
+import com.example.fixturesmanage.database.FixturesManageDatabase
 import com.example.fixturesmanage.databinding.FragmentUnitBinding
 
 class UnitFragment : Fragment() {
+
+    lateinit var mUnitDao: UnitDao
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        mUnitDao = FixturesManageDatabase.getInstance(this.requireContext()).unitDao()
         val binding = DataBindingUtil.inflate<FragmentUnitBinding>(inflater, R.layout.fragment_unit, container, false)
 
         binding.createUnit.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_nav_unit_to_unitCreateFragment)
         }
         setHasOptionsMenu(true)
+
+        // Adapterに渡す配列を作成します
+        val units = mUnitDao.getAll() //.map { u -> "${u.id}: ${u.name}" }
+        // 一覧の作成
+        val listView = binding.unitList
+        val unitAdapter = UnitAdapter(requireContext(), R.layout.list_unit, units)
+        listView.adapter = unitAdapter
+
+        // listViewの行がクリックされた時のイベントリスナー
+        listView.setOnItemClickListener {parent, view, position, id ->
+            // listViewのクリックされた行のテキストを取得
+            val id = view.findViewById<TextView>(R.id.textViewId).text
+            val name = view.findViewById<TextView>(R.id.textViewName).text
+            // トーストで表示する
+            Toast.makeText(requireContext(), "${id}: $name", Toast.LENGTH_LONG).show()
+
+            // 生成されたクラスに引数を渡して遷移
+            val action = UnitFragmentDirections.actionNavUnitToUnitShowFragment(id.toString().toInt())
+            view.findNavController().navigate(action)
+        }
+
         return binding.root
     }
 }
