1c4b900f81c23dde584a92907ecafe75c2b025f2
2ndwave-Kobayashi <kobayashi@2ndwave.jp>
2021-05-21 11:13:18 +0900

    単位マスタ：詳細画面、削除処理の作成

---
+++ app/src/main/java/com/example/fixturesmanage/ui/unit/UnitShowFragment.kt
@@ -0,0 +1,50 @@
+package com.example.fixturesmanage.ui.unit
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.fixturesmanage.R
+import com.example.fixturesmanage.dao.UnitDao
+import com.example.fixturesmanage.database.FixturesManageDatabase
+import com.example.fixturesmanage.databinding.ShowUnitFragmentBinding
+
+class UnitShowFragment : Fragment() {
+
+    lateinit var mUnitDao: UnitDao
+    private val args: UnitShowFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mUnitDao = FixturesManageDatabase.getInstance(this.requireContext()).unitDao()
+        val binding = DataBindingUtil.inflate<ShowUnitFragmentBinding>(inflater, R.layout.show_unit_fragment, container, false)
+
+        // 表示するモデルを取得
+        val unit = mUnitDao.findUnit(args.selectedId)
+
+        binding.textViewName.text = unit.name
+
+        binding.buttonDelete.setOnClickListener { view : View ->
+            AlertDialog.Builder(this.requireContext()) // FragmentではActivityを取得して生成
+                .setTitle("削除確認")
+                .setMessage("${unit.name} を削除しますか？")
+                .setPositiveButton("OK") { dialog, which ->
+                    mUnitDao.delete(unit)
+                    view.findNavController().navigate(R.id.action_unitShowFragment_to_nav_unit)
+                }
+                .setNegativeButton("No") { dialog, which -> /* 何もしない */ }
+                .show()
+        }
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+}
