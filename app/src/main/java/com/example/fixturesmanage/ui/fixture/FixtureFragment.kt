0dfc84d45d020ade4934154f1c9bb427e9a23c2c
2ndwave-Kobayashi <kobayashi@2ndwave.jp>
2021-05-24 10:57:24 +0900

    備品マスタ：検索機能追加

--- app/src/main/java/com/example/fixturesmanage/ui/fixture/FixtureFragment.kt
+++ app/src/main/java/com/example/fixturesmanage/ui/fixture/FixtureFragment.kt
@@ -1,10 +1,15 @@
 package com.example.fixturesmanage.ui.fixture
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -13,7 +18,6 @@ import com.example.fixturesmanage.adapter.FixtureAdapter
 import com.example.fixturesmanage.dao.FixtureDao
 import com.example.fixturesmanage.database.FixturesManageDatabase
 import com.example.fixturesmanage.databinding.FragmentFixtureBinding
-import com.example.fixturesmanage.ui.type.TypeFragmentDirections
 
 class FixtureFragment : Fragment() {
 
@@ -24,6 +28,12 @@ class FixtureFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        /* 開かれたタイミングでキーボードを閉じる */
+        val inputManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (container != null) {
+            inputManager.hideSoftInputFromWindow(container.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+
         mFixtureDao = FixturesManageDatabase.getInstance(this.requireContext()).fixtureDao()
         val binding = DataBindingUtil.inflate<FragmentFixtureBinding>(inflater, R.layout.fragment_fixture, container, false)
 
@@ -33,10 +43,10 @@ class FixtureFragment : Fragment() {
         setHasOptionsMenu(true)
 
         // Adapterに渡す配列を作成します
-        val fixtures = mFixtureDao.getAll() //.map { u -> "${u.id}: ${u.name}" }
+        var fixtures = mFixtureDao.getAll() //.map { u -> "${u.id}: ${u.name}" }
         // 一覧の作成
         val listView = binding.fixtureList
-        val fixtureAdapter = FixtureAdapter(requireContext(), R.layout.list_fixture, fixtures)
+        var fixtureAdapter = FixtureAdapter(requireContext(), R.layout.list_fixture, fixtures)
         listView.adapter = fixtureAdapter
 
         // listViewの行がクリックされた時のイベントリスナー
@@ -46,6 +56,31 @@ class FixtureFragment : Fragment() {
             // 生成されたクラスに引数を渡して遷移
             val action = FixtureFragmentDirections.actionNavFixtureToFixtureShowFragment(id.toString().toInt())
             view.findNavController().navigate(action)
+        }
+
+        binding.editSearchName.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                listView.visibility = View.VISIBLE
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                listView.visibility = View.INVISIBLE
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                var searchText = binding.editSearchName.text
+                fixtures = mFixtureDao.includeName("%${searchText}%")
+                fixtureAdapter = FixtureAdapter(requireContext(), R.layout.list_unit, fixtures)
+                listView.adapter = fixtureAdapter
+            }
+        })
+        binding.buttonSearch.setOnClickListener { view : View ->
+            var searchText = binding.editSearchName.text
+            fixtures = mFixtureDao.includeName("%${searchText}%")
+            fixtureAdapter = FixtureAdapter(requireContext(), R.layout.list_unit, fixtures)
+            listView.adapter = fixtureAdapter
+
+            /* ボタンクリックのタイミングでキーボードを閉じる */
+            val inputManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
         }
 
         return binding.root
